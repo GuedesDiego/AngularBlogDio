778cf998ef292bb8926fd9d822d6e8ed5c078911
GuedesDiego <sevenco.informatica@gmail.com>
2022-11-21 12:11:33 -0300

    Blog finalizado

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -8,10 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BigCardComponent implements OnInit {
   @Input()
   photoCover: string =""
+
   @Input()
   cardTitle: string =""
+
   @Input()
   cardDescription: string =""
+
+  @Input()
+  id:string="0"
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+{
+    "id":"1",
+    "photo":"https://64.media.tumblr.com/3a4c5b10ae352525d0e30096c2a2643e/edaa676252634113-62/s1280x1920/30790eecc7f43eca9ddfa67394aeb5f011271d6c.jpg",
+    "title":"O CAVEIRA MAIS TEMIDO DA FACE DA TERRA",
+    "description":"ARE YOU READY?",
+    
+},
+{
+    "id":"2",
+    "photo":"https://wallpaperaccess.com/full/8203892.jpg",
+    "title":"E aí ? Vai encarar?",
+    "description":"SEVEN7"
+},
+
+{
+    "id":"3",
+    "photo":"https://wallpaperaccess.com/full/8203883.jpg",
+    "title":"Ele será o seu pior pesadelo!",
+    "description":" SEVEN7"
+},
+
+{
+    "id":"4",
+    "photo":"https://wallpaperaccess.com/full/8203912.jpg",
+    "title":"A sua hora vai chegar price! hahahahahahah!",
+    "description":" SEVEN7"
+},
+]
+
+
+
+
+   
